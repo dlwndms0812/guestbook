647449b9ee97d38689a26c0ecd9464a07b17aa32
dlwndms0812 <dlwndms0812@naver.com>
2022-03-11 15:06:10 +0900

    방명록의 조회 처리

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -2,12 +2,14 @@ package com.example.guestbook.controller;
 
 import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.entity.Guestbook;
 import com.example.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -45,5 +47,13 @@ public class GuestbookController {
         redirectAttributes.addFlashAttribute("msg",gno);
 
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO,Model model){
+        log.info("gno: "+gno);
+
+        GuestbookDTO dto=service.read(gno);
+        model.addAttribute("dto",dto);
     }
 }
